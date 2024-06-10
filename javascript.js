360bda97fb86226d0a8762cee07af12aba7e35bd
himanshuthakur009 <htthakur96@gmail.com>
2024-06-10 12:31:23 +0530

    I have created a file that exits as a js file in my git repo

---
+++ javascript.js
@@ -0,0 +1,4 @@
+//i am just telling my browser to let knw i have create my own js file in git 
+//ntng  serious just tell the browser to tell everyone to that this is going to be crazy
+
+console.log("HELLO WORLD");
